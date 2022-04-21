f962c0ae399652bcb6609fa92f31dec835625b3c
Zach Rickers <mzrickers@gmail.com>
2022-04-21 09:23:09 +0200

    added objects

--- send_email.py
+++ send_email.py
@@ -2,3 +2,14 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 
+subject = "Email from Python"
+body = "This is a test email from Python!"
+sender_email = "mzrickers@gmail.com"
+receiver_email = "mzrickers@gmail.com"
+password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
