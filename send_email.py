2a86819cb2d95f99d9764a1bb5c6448ca142aaf2
Zach Rickers <mzrickers@gmail.com>
2022-04-21 09:30:14 +0200

    added html

--- send_email.py
+++ send_email.py
@@ -14,8 +14,20 @@ message["To"] = receiver_email
 message["Subject"] = subject
 message.set_content(body)
 
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
 context = ssl.create_default_context()
 
+print("Sending Email!")
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message.as_string())
+print("Success!")
+
