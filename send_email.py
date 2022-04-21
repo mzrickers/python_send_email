bad1b142ac77f9c18b7a735df1a6fa0f530b715d
Zach Rickers <mzrickers@gmail.com>
2022-04-21 09:27:52 +0200

    first test

--- send_email.py
+++ send_email.py
@@ -17,4 +17,5 @@ message.set_content(body)
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
